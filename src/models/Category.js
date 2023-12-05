f4dd5f43288c4fc7761d65322ebf864bb680ffc6
facuforipa01 <facufolatelli2001@gmail.com>
2023-12-05 17:02:21 -0300

    estructura

---
+++ src/models/Category.js
@@ -0,0 +1,12 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("./connect");
+
+const Category = sequelize.define("Category", {
+
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+});
+
+module.exports = Category;
